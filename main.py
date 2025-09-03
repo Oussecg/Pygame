69c953d410e8ab69c0cd32090301769df0e95c92
Oussecg <anasoussema16@gmail.com>
2025-09-03 15:39:52 +0100

    We add jump to our player

--- main.py
+++ main.py
@@ -9,20 +9,21 @@ pygame.init()
 running = True
 
 while running:
-    # poll for events
+    # fill the screen with a color to wipe away anything from last frame
+    game.screen.fill("white")
+
+    # RENDER YOUR GAME HERE
+    game.screen.blit(game.background, (game.background_rect.x, game.background_rect.y))
+    game.screen.blit(game.player.img, (game.player.rect.x, game.player.rect.y))
+    game.player.update()
 
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    game.screen.fill("white")
-
-    # RENDER YOUR GAME HERE
-    game.screen.blit(game.background, (game.background_rect.x, game.background_rect.y ))
-    game.screen.blit(game.player.img, (game.player.rect.x, game.player.rect.y))
-    game.player.move()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                game.player.on_jump = True
 
     # flip() the display to put your work on screen
     pygame.display.flip()
--- player.py
+++ player.py
@@ -8,12 +8,34 @@ class Player(pygame.sprite.Sprite):
         self.img = pygame.transform.scale(self.img, (50, 50))
         self.rect = self.img.get_rect()
         self.rect.x = 0
-        self.rect.y = pygame.display.get_surface().get_height() - ((self.background.get_height() // 2) + self.img.get_height())
+        self.initial_y = pygame.display.get_surface().get_height() - ((self.background.get_height() // 2) + self.img.get_height())
+        self.rect.y = self.initial_y
         self.speed_x = 5
         self.speed_y = 5
+        self.jump_strength = 8
+        self.gravity_power = 5
+        self.on_jump = False
 
     def move(self):
         if self.rect.x + self.img.get_width() <= pygame.display.get_surface().get_width():
             self.rect.x += self.speed_x
         else:
             self.rect.x = 0
+
+    def jump(self):
+        if self.on_jump:
+            if self.rect.y >= self.initial_y - 150:
+                self.speed_x = 9
+                self.rect.y -= self.jump_strength
+            else:
+                self.speed_x = 5
+                self.on_jump = False
+
+    def gravity(self):
+        if (self.on_jump == False) and (self.rect.y < self.initial_y):
+            self.rect.y += self.gravity_power
+
+    def update(self):
+        self.move()
+        self.jump()
+        self.gravity()
